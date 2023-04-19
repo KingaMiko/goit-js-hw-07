b70f314c596ee93cfa2b65079413226ec7e775a5
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-04-19 13:02:59 +0200

    gallery without function

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const imageMarkup = createMarkup(galleryItems);
+gallery.insertAdjacentHTML("beforeend", imageMarkup);
+
+function createMarkup(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<ul class = "gallery"> <li> <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a> </li> </ul>`;
+    })
+    .join("");
+}
 
 console.log(galleryItems);
