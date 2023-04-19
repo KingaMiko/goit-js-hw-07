97fa8b726b76333f00823bb2ab16308648897184
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-04-19 11:38:31 +0200

    First task completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const imageMarkup = createMarkup(galleryItems);
+gallery.insertAdjacentHTML("beforeend", imageMarkup);
+
+function createMarkup(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+const selectImage = (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const source = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+  <img src="${source}" width="800" height="600">
+`);
+  instance.show();
+
+  const keyClose = (event) => {
+    if (event.key === "Escape" && instance.visible()) {
+      instance.close();
+    }
+  };
+  document.addEventListener("keydown", keyClose);
+};
+
+gallery.addEventListener("click", selectImage);
 
 console.log(galleryItems);
