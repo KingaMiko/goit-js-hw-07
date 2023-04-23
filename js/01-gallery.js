e29cb77f836602fb51e2adf4d09f8c5dd75c4e5f
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-04-23 10:31:58 +0200

    another 2 method to resolve task 1 added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,8 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const gallery = document.querySelector(".gallery");
+
+//Method 1
 const imageMarkup = createMarkup(galleryItems);
 gallery.insertAdjacentHTML("beforeend", imageMarkup);
 
@@ -21,6 +23,55 @@ function createMarkup() {
     .join("");
 }
 
+//Method 2
+// galleryItems.forEach((item) => {
+//   const galleryContainer = document.createElement("li");
+//   galleryContainer.classList.add("gallery__item");
+//   gallery.append(galleryContainer);
+
+//   const innerBox = document.createElement("a");
+//   innerBox.classList.add("gallery__link");
+//   innerBox.href = item.original;
+//   galleryContainer.append(innerBox);
+
+//   const imageBox = document.createElement("img");
+//   imageBox.textContent = item;
+//   imageBox.src = item.preview;
+//   imageBox.alt = item.description;
+//   imageBox.dataset.source = item.original;
+//   imageBox.classList.add("gallery__image");
+//   innerBox.append(imageBox);
+// });
+
+//Method 3
+// function galleryCreate() {
+//   let galleryArray = [];
+
+//   galleryItems.forEach((el, index) => {
+//     const { description, original, preview } = galleryItems[index];
+//     const li = document.createElement("li");
+//     const a = document.createElement("a");
+//     const img = document.createElement("img");
+
+//     li.classList.add("gallery__item");
+//     a.classList.add("gallery__link");
+//     img.classList.add("gallery__image");
+
+//     a.setAttribute("href", original);
+//     img.setAttribute("src", preview);
+//     img.setAttribute("data-source", original);
+//     img.setAttribute("alt", description);
+
+//     li.append(a);
+//     a.append(img);
+
+//     galleryArray.push(li);
+//   });
+//   gallery.append(...galleryArray);
+// }
+// galleryCreate();
+
+// Method 1
 // const selectImage = (event) => {
 //   event.preventDefault();
 //   if (event.target.nodeName !== "IMG") {
@@ -40,6 +91,7 @@ function createMarkup() {
 //   document.addEventListener("keydown", keyClose);
 // };
 
+//Method 2
 const selectImage = (event) => {
   event.preventDefault();
   const source =
